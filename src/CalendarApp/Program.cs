540409492bb06ac156cbc7b769a136f61d13d806
Hristijan Zdravev <zdrhristijan@gmail.com>
2025-11-29 14:39:37 +0100

    added swagger and fixed startup

--- src/CalendarApp/Program.cs
+++ src/CalendarApp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using CalendarApp;
+using CalendarApp.Repository;
 using CalendarApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,11 @@ builder.Services.AddHttpClient();
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<ConsoleLogger>();
 builder.Services.AddSingleton<IEventExpander, EventExpander>();
+builder.Services.AddScoped<IEventRepository>(sp =>
+{
+    var logger = sp.GetRequiredService<ConsoleLogger>();
+    return new FakeEventRepository(logger);
+});
 builder.Services.AddScoped<CalendarScheduler>();
 
 // Register the exporter with the token from config
@@ -34,7 +41,24 @@ builder.Services.AddScoped<ICalendarExporter>(sp =>
 // Add controller support
 builder.Services.AddControllers();
 
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Calendar API", Version = "v1" });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calendar API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
 app.MapControllers();
 
